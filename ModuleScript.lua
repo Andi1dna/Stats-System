50f541d2c5497541c591d46ca073c38407e8954b
djeku123441 <99770925+djeku123441@users.noreply.github.com>
2025-02-12 02:17:04 +0100

    Create ModuleScript.lua
    This script should be put in the module side

---
+++ ModuleScript.lua
@@ -0,0 +1,16 @@
+local module = {
+	["Z"] = {
+		["Damage"] = 40,
+		["Cooldown"] = 9
+	},
+	["C"] = {
+		["Health"] = 1,
+		["Cooldown"] = 14
+	},
+	["V"] = {
+		["Cooldown"] = 49
+	}
+		
+}
+
+return module
