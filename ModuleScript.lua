6b40922bb3be3271f99617b51ef14e10834a9b5f
Andi <99770925+Andi1dna@users.noreply.github.com>
2025-09-08 10:29:27 +0200

    Update ModuleScript.lua

--- ModuleScript.lua
+++ ModuleScript.lua
@@ -1,16 +1,24 @@
 local module = {
-	["Z"] = {
-		["Damage"] = 40,
-		["Cooldown"] = 9
+	["Speed"] = {
+		["MinimumSpeed"] = 8,
+		["MaxSpeed"] = 50
 	},
-	["C"] = {
-		["Health"] = 1,
-		["Cooldown"] = 14
+	["Durability"] = {
+		["MinimumHealth"] = 1,
+		["MaximumHealth"] = 1000
 	},
-	["V"] = {
-		["Cooldown"] = 49
+	["Strength"] = {
+		["MinimumStr"] = 1,
+		["MaximumStr"] = 950
+	},
+	["Level"] = {
+		["MinimumLevel"] = 0,
+		["MaximumLevel"] = 250
+	},
+	["StatPoints"] = {
+		["MinimumStatPoints"] = 0,
+		["MaximumStatPoints"] = 2000
 	}
-		
 }
 
 return module
